2993a5552a0f11e7da481168862ba6eccebd8480
Ferdinand Bauer <f.bau3r@gmail.com>
2023-11-18 23:00:02 +0100

    feat: setup project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Xbim.Ifc;
+
+const string fileName = "SampleHouse.ifc";
+
+var editor = new XbimEditorCredentials
+{
+    ApplicationDevelopersName = "Ferdi",
+    ApplicationFullName = "",
+    ApplicationIdentifier = "",
+    ApplicationVersion = "",
+    //your user
+    EditorsFamilyName = "Fuchs",
+    EditorsGivenName = "",
+    EditorsOrganisationName = ""
+};
+
+using (IfcStore model = IfcStore.Open(fileName, editor))
+{
+    using (var txn = model.BeginTransaction("Quick start transaction"))
+    {
+        txn.Commit();
+    }
+    model.SaveAs("SampleHouse_Modified.ifc");
+}
